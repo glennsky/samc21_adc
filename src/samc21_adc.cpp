0517b45385f0a3c8d63e85c8de8e655f94ad3921
Scott Price <prices@hugllc.com>
2017-12-28 16:06:40 -0600

    Added routines to set the reference, as well as the averager.

--- src/samc21_adc.cpp
+++ src/samc21_adc.cpp
@@ -9,23 +9,80 @@
 #include "samc21_adc.h"
 #include "Arduino.h"
 
-/**
-* @brief Checks for the presence of a connector id
-*
-* This function never returns if there is no connector id.
-*
-* @return void
-*/
+#ifndef ADC_INPUTCTRL_MUXNEG_GND
+#define ADC_INPUTCTRL_MUXNEG_GND (0x18ul << ADC_INPUTCTRL_MUXNEG_Pos)
+#endif
+
 SAMC21_ADC::SAMC21_ADC(Adc* Conv)
 : _adc(Conv)
 {
 };
 
-uint8_t SAMC21_ADC::begin(void)
+uint8_t SAMC21_ADC::begin(samc21_adc_ref vref)
 {
+    uint32_t biasrefbuf = 0;
+    uint32_t biascomp = 0;
+    if (_adc != NULL) {
+        if (_adc == ADC0) {
+            MCLK->APBCMASK.reg |= MCLK_APBCMASK_ADC0;
+            biasrefbuf = (*((uint32_t *) ADC0_FUSES_BIASREFBUF_ADDR) & ADC0_FUSES_BIASREFBUF_Msk) >> ADC0_FUSES_BIASREFBUF_Pos;
+            biascomp   = (*((uint32_t *) ADC0_FUSES_BIASCOMP_ADDR) & ADC0_FUSES_BIASCOMP_Msk) >> ADC0_FUSES_BIASCOMP_Pos;
+        } else if (_adc == ADC1) {
+            MCLK->APBCMASK.reg |= MCLK_APBCMASK_ADC1;
+            biasrefbuf = (*((uint32_t *) ADC1_FUSES_BIASREFBUF_ADDR) & ADC1_FUSES_BIASREFBUF_Msk) >> ADC1_FUSES_BIASREFBUF_Pos;
+            biascomp   = (*((uint32_t *) ADC1_FUSES_BIASCOMP_ADDR) & ADC1_FUSES_BIASCOMP_Msk) >> ADC1_FUSES_BIASCOMP_Pos;
+        }
     
+        _adc->CALIB.reg = ADC_CALIB_BIASREFBUF(biasrefbuf) | ADC_CALIB_BIASCOMP(biascomp);
+
+        _adc->CTRLB.reg = ADC_CTRLB_PRESCALER_DIV256;
+        _adc->CTRLC.reg = ADC_CTRLC_RESSEL_12BIT | ADC_CTRLC_R2R;
+
+        _adc->SAMPCTRL.reg = (ADC_SAMPCTRL_SAMPLEN(0x0) | ADC_SAMPCTRL_OFFCOMP);
+        _sync();
+
+        _adc->INPUTCTRL.reg = ADC_INPUTCTRL_MUXNEG_GND;
+        _sync();
+
+        average();
+        ref(vref);
+    } else {
+        return 1;
+    }
+
     return 0;
 };
+
+void SAMC21_ADC::average(samc21_adc_avg_samples samples, samc21_adc_avg_divisor div)
+{
+    _sync();
+    _adc->AVGCTRL.reg = samples | div;
+}
+
+void SAMC21_ADC::ref(samc21_adc_ref vref)
+{
+    _sync();
+    switch (vref) {
+        case 0:
+        case 2:
+        case 3:
+            SUPC->VREF.reg &= ~SUPC_VREF_SEL_Msk;
+            SUPC->VREF.reg |= SUPC_VREF_SEL(vref);
+            _adc->REFCTRL.reg &= ~ADC_REFCTRL_REFSEL_Msk;
+    }
+};
+
+
+/**
+ * This synchronizes the clocks.
+ * 
+ * @return void
+ */
+void SAMC21_ADC::_sync(void)
+{
+  while ( _adc->SYNCBUSY.reg & ADC_SYNCBUSY_MASK );
+}
+
 /**
  * The handler for ADC0
  * 
--- src/samc21_adc.h
+++ src/samc21_adc.h
@@ -13,24 +13,84 @@
 #include <inttypes.h>
 #include <string.h>
 
+enum samc21_adc_ref
+{
+    SAMC21_ADC_REF_1024 = 0,
+    SAMC21_ADC_REF_2048 = 2,
+    SAMC21_ADC_REF_4096 = 3,
+};
+
+enum samc21_adc_avg_samples
+{
+    SAMC21_ADC_AVGSAMPLES_1    = ADC_AVGCTRL_SAMPLENUM_1,
+    SAMC21_ADC_AVGSAMPLES_2    = ADC_AVGCTRL_SAMPLENUM_2,
+    SAMC21_ADC_AVGSAMPLES_4    = ADC_AVGCTRL_SAMPLENUM_4,
+    SAMC21_ADC_AVGSAMPLES_8    = ADC_AVGCTRL_SAMPLENUM_8,
+    SAMC21_ADC_AVGSAMPLES_16   = ADC_AVGCTRL_SAMPLENUM_16,
+    SAMC21_ADC_AVGSAMPLES_32   = ADC_AVGCTRL_SAMPLENUM_32,
+    SAMC21_ADC_AVGSAMPLES_64   = ADC_AVGCTRL_SAMPLENUM_64,
+    SAMC21_ADC_AVGSAMPLES_128  = ADC_AVGCTRL_SAMPLENUM_128,
+    SAMC21_ADC_AVGSAMPLES_256  = ADC_AVGCTRL_SAMPLENUM_256,
+    SAMC21_ADC_AVGSAMPLES_512  = ADC_AVGCTRL_SAMPLENUM_512,
+    SAMC21_ADC_AVGSAMPLES_1024 = ADC_AVGCTRL_SAMPLENUM_1024,
+};
+
+enum samc21_adc_avg_divisor
+{
+    SAMC21_ADC_AVGDIV_1   = ADC_AVGCTRL_ADJRES(0),
+    SAMC21_ADC_AVGDIV_2   = ADC_AVGCTRL_ADJRES(1),
+    SAMC21_ADC_AVGDIV_4   = ADC_AVGCTRL_ADJRES(2),
+    SAMC21_ADC_AVGDIV_8   = ADC_AVGCTRL_ADJRES(3),
+    SAMC21_ADC_AVGDIV_16  = ADC_AVGCTRL_ADJRES(4),
+    SAMC21_ADC_AVGDIV_32  = ADC_AVGCTRL_ADJRES(5),
+    SAMC21_ADC_AVGDIV_64  = ADC_AVGCTRL_ADJRES(6),
+    SAMC21_ADC_AVGDIV_128 = ADC_AVGCTRL_ADJRES(7),
+};
 
 class SAMC21_ADC
 {
 
 public:
     /**
-    * @brief Checks for the presence of a connector id
-    *
-    * This function never returns if there is no connector id.
+    * @brief Creates the ADC object.
+    * 
+    * @param *Conv The ADC pointer to use.  Should be ADC0 or ADC1.
     *
     * @return void
     */
     SAMC21_ADC(Adc *Conv);
-    uint8_t begin(void);
+    /**
+    * @brief Sets up the ADC
+    * 
+    * @param *Conv The ADC pointer to use.  Should be ADC0 or ADC1.
+    *
+    * @return void
+    */
+    uint8_t begin(samc21_adc_ref vref = SAMC21_ADC_REF_1024);
+    /**
+    * @brief Sets the reference
+    * 
+    * @param vref The reference to use.
+    *
+    * @return void
+    */
+    void ref(samc21_adc_ref vref);
+    /**
+    * @brief Sets the averager
+    * 
+    * @param samples The number of samples.
+    * @param div     The divisor.
+    *
+    * @return void
+    */
+    void average(samc21_adc_avg_samples samples = SAMC21_ADC_AVGSAMPLES_1, samc21_adc_avg_divisor div = SAMC21_ADC_AVGDIV_1);
+
 
 private:
     Adc* _adc;
     
+    void _sync(void);
+
 };
 
 
