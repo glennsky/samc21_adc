4e001a425eb3a31498e575e2cea030a764bd9bef
Scott Price <prices@hugllc.com>
2018-03-22 10:48:44 -0500

    Added an averager to the interrupts, so they can average after the fact, also.

--- src/samc21_adc.cpp
+++ src/samc21_adc.cpp
@@ -257,14 +257,34 @@ int32_t SAMC21_ADC::value(void)
  */
 void ADC0_Handler(void)
 {
+#ifdef ADC0_AVERAGE_BITS 
+    volatile static int32_t  acc = 0;
+    volatile static uint16_t cnt = 0;
+    int32_t read;
+#endif
     uint8_t flags = ADC0->INTFLAG.reg;
     if (samc21_adc_obj[0] != NULL) {
         if ((flags & ADC_INTFLAG_RESRDY) == ADC_INTFLAG_RESRDY) {
+#ifdef ADC0_AVERAGE_BITS 
+            acc += (int16_t)ADC0->RESULT.reg;
+            cnt++;
+            if (cnt >= (1 << ADC0_AVERAGE_BITS)) {
+                read = (acc >> (ADC0_AVERAGE_BITS - 4));
+                if (samc21_adc0_callback != NULL) {
+                    samc21_adc0_callback(samc21_adc_obj[0], read, (uint8_t)ADC0->SEQSTATUS.bit.SEQSTATE, samc21_adc0_callback_ptr);
+                } else {
+                    samc21_adc_obj[0]->addNew(read, (uint8_t)ADC0->SEQSTATUS.bit.SEQSTATE);
+                }
+                acc = 0;
+                cnt = 0;
+            }
+#else
             if (samc21_adc0_callback != NULL) {
                 samc21_adc0_callback(samc21_adc_obj[0], ADC0->RESULT.reg, (uint8_t)ADC0->SEQSTATUS.bit.SEQSTATE, samc21_adc0_callback_ptr);
             } else {
                 samc21_adc_obj[0]->addNew(ADC0->RESULT.reg, (uint8_t)ADC0->SEQSTATUS.bit.SEQSTATE);
             }
+#endif
         }
         if ((flags & ADC_INTFLAG_WINMON) == ADC_INTFLAG_WINMON) {
             samc21_adc_obj[0]->window();
@@ -281,14 +301,34 @@ void ADC0_Handler(void)
  */
 void ADC1_Handler(void)
 {
+#ifdef ADC1_AVERAGE_BITS 
+    volatile static int32_t  acc = 0;
+    volatile static uint16_t cnt = 0;
+    int32_t read;
+#endif
     uint8_t flags = ADC1->INTFLAG.reg;
     if (samc21_adc_obj[1] != NULL) {
         if ((flags & ADC_INTFLAG_RESRDY) == ADC_INTFLAG_RESRDY) {
+#ifdef ADC1_AVERAGE_BITS 
+            acc += (int16_t)ADC1->RESULT.reg;
+            cnt++;
+            if (cnt >= (1 << ADC1_AVERAGE_BITS)) {
+                read = (acc >> (ADC1_AVERAGE_BITS - 4));
+                if (samc21_adc1_callback != NULL) {
+                    samc21_adc1_callback(samc21_adc_obj[1], read, (uint8_t)ADC1->SEQSTATUS.bit.SEQSTATE, samc21_adc1_callback_ptr);
+                } else {
+                    samc21_adc_obj[1]->addNew(read, (uint8_t)ADC1->SEQSTATUS.bit.SEQSTATE);
+                }
+                acc = 0;
+                cnt = 0;
+            }
+#else
             if (samc21_adc1_callback != NULL) {
                 samc21_adc1_callback(samc21_adc_obj[1], ADC1->RESULT.reg, (uint8_t)ADC1->SEQSTATUS.bit.SEQSTATE, samc21_adc1_callback_ptr);
             } else {
                 samc21_adc_obj[1]->addNew(ADC1->RESULT.reg, (uint8_t)ADC1->SEQSTATUS.bit.SEQSTATE);
             }
+#endif
         }
         if ((flags & ADC_INTFLAG_WINMON) == ADC_INTFLAG_WINMON) {
             samc21_adc_obj[1]->window();
